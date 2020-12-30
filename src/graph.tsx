8e6cab84f94413965b2d8411b867ee5165d70d16
Igor Muntyan <igor.muntyan@gmail.com>
2020-12-30 13:32:15 -0500

    graph changes

--- src/graph.tsx
+++ src/graph.tsx
@@ -13,14 +13,15 @@ class Graph extends React.Component {
     render() {
         console.log(this.props.chart)
         return (
-            <div className="Graph" style={{ height: 400, width: 600 }}>
+            <div className="Graph" style={{ height: 400, width: 600, backgroundColor: "white" }}>
+
                 <ResponsiveBar
                     data={this.props.chart}
                     keys={[ 'probability' ]}
                     indexBy="digit"
-                    margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+                    margin={{ top: 50, right: 60, bottom: 50, left: 80 }}
                     padding={0.3}
-                    valueScale={{ type: 'linear' }}
+                    valueScale={{ type: 'linear', min: 0, max: 1 }}
                     indexScale={{ type: 'band', round: true }}
                     colors={{ scheme: 'nivo' }}
                     borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
@@ -40,8 +41,10 @@ class Graph extends React.Component {
                         tickRotation: 0,
                         legend: 'probability',
                         legendPosition: 'middle',
-                        legendOffset: 40
+                        legendOffset: -40,
+                        tickValues: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
                     }}
+                    enableLabel={false}
                     labelSkipWidth={12}
                     labelSkipHeight={12}
                     labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
